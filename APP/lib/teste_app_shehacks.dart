ed6262b281fd108ca6b62880d35ddb5f9eab30ea
juliana.mitiehn <juliana.mitiehn@usp.br>
2023-11-11 09:22:23 -0300

    :rocket: first commit

---
+++ APP/lib/teste_app_shehacks.dart
@@ -0,0 +1,27 @@
+// Container(
+// color: Colors.white,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(color: Colors.indigo, width: 100, height: 100),
+// Container(color: Colors.redAccent, width: 50, height: 50),
+// Container(
+// color: Colors.amber,
+// child: Text(
+// 'teste aaaaaaaaaa',
+// style: TextStyle(
+// color: Colors.black,
+// fontSize: 20,
+// ),
+// textAlign: TextAlign.center,
+// ),
+// ),
+// ElevatedButton(
+// onPressed: () {
+// print('apertou botao');
+// },
+// child: Text('Botão'))
+// ],
+// ),
+// )
